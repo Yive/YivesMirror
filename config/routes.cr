049317586ea2083d8a9abf692c3c1ed7d09a873f
Yive <admin@yive.me>
2018-11-12 05:34:00 -0800

    Unused plug + Fixes compile error

--- config/routes.cr
+++ config/routes.cr
@@ -4,7 +4,6 @@ Amber::Server.configure do
     # A plug accepts an instance of HTTP::Handler
     plug Amber::Pipe::PoweredByAmber.new
     # plug Amber::Pipe::ClientIp.new(["X-Forwarded-For"])
-    plug Citrine::I18n::Handler.new
     plug Amber::Pipe::Error.new
     plug Amber::Pipe::Logger.new
     plug Amber::Pipe::CSRF.new
